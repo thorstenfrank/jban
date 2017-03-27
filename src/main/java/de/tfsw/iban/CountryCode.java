a6c72622bbbb5c7307705992e26a7646a729ec5e
Thorsten Frank <thorsten@tfsw.de>
2017-03-27 20:09:05 +0200

    Add support for segmented bank and branch identifier patterns

--- src/main/java/de/tfsw/iban/CountryCode.java
+++ src/main/java/de/tfsw/iban/CountryCode.java
@@ -26,98 +26,311 @@ package de.tfsw.iban;
  */
 public enum CountryCode {
 
-    AD(20, "\\d{4}\\d{4}\\w{12}"), // Andorra 
-    AE(19, "\\d{3}\\d{16}"), // United Arab Emirates 
-    AL(24, "\\d{8}\\w{16}"), // Albania 
-    AT(16, "\\d{5}\\d{11}"), // Austria
-    AZ(24, "[A-Z]{4}\\w{20}"), // Azerbaijan 
-    BA(16, "\\d{3}\\d{3}\\d{8}\\d{2}"), // Bosnia and Hergegovina 
-    BE(12, "\\d{3}\\d{7}\\d{2}"), // Belgium
-    BG(18, "[A-Z]{4}\\d{4}\\d{2}\\w{8}"), // Bulgaria
-    BH(18, "[A-Z]{4}\\w{14}"), // Bahrain
-    BR(25, "\\d{8}\\d{5}\\d{10}[A-Z]\\w"), // Brazil
-    BY(24, "\\w{4}\\d{4}\\w{16}"), // Belarus
-    CH(17, "\\d{5}\\w{12}"), // Switzerland
-    CR(18, "\\d{4}\\d{14}"), // Costa Rica
-    CY(24, "\\d{3}\\d{5}\\w{16}"), // Cyprus 
-    CZ(20, "\\d{4}\\d{6}\\d{10}"), // Czech Republic
-    DE(18, "\\d{8}\\d{10}"), // Germany
-    DK(14, "\\d{4}\\d{9}\\d{1}"), // Denmark
-    DO(24, "\\w{4}\\d{20}"), // Dominican Republic
-    EE(16, "\\d{2}\\d{2}\\d{11}\\d"), // Estonia
-    ES(20, "\\d{4}\\d{4}\\d\\d\\d{10}"), // Spain
-    FI(14, "\\d{3}\\d{11}"), // Finland
-    FO(14, "\\d{4}\\d{9}\\d"), // Faroe Islands
-    FR(23, "\\d{5}\\d{5}\\w{11}\\d{2}"), // France
-    GB(18, "[A-Z]{4}\\d{6}\\d{8}"), // United Kingdom
-    GE(18, "[A-Z]{2}\\d{16}"), // Georgia
-    GI(19, "[A-Z]{4}\\w{15}"), // Gibraltar
-    GL(14, "\\d{4}\\d{9}\\d"), // Greenland
-    GR(23, "\\d{3}\\d{4}\\w{16}"), // Greece
-    GT(24, "\\w{4}\\w{20}"), // Guatemala
-    HR(17, "\\d{7}\\d{10}"), // Croatia
-    HU(24, "\\d{3}\\d{4}\\d\\d{15}\\d"), // Hungary
-    IE(18, "[A-Z]{4}\\d{6}\\d{8}"), // Ireland
-    IL(19, "\\d{3}\\d{3}\\d{13}"), // Israel
-    IQ(19, "[A-Z]{4}\\d{3}\\d{12}"), // Iraq
-    IS(22, "\\d{4}\\d{2}\\d{6}\\d{10}"), // Iceland
-    IT(23, "[A-Z]\\d{5}\\d{5}\\w{12}"), // Italy
-    JO(26, "[A-Z]{4}\\d{4}\\w{18}"), // Jordan
-    KW(26, "[A-Z]{4}\\w{22}"), // Kuwait
-    KZ(16, "\\d{3}\\w{13}"), // Kazakhstan
-    LB(24, "\\d{4}\\w{20}"), // Lebanon
-    LC(28, "[A-Z]{4}\\w{24}"), // Saint Lucia
-    LI(17, "\\d{5}\\w{12}"), // Liechtenstein
-    LT(16, "\\d{5}\\d{11}"), // Lithuania
-    LU(16, "\\d{3}\\w{13}"), // Luxembourg
-    LV(17, "[A-Z]{4}\\w{13}"), // Latvia
-    MC(23, "\\d{5}\\d{5}\\w{11}\\d{2}"), // Monaco
-    MD(20, "\\w{2}\\w{18}"), // Moldova
-    ME(18, "\\d{3}\\d{13}\\d{2}"), // Montenegro
-    MK(15, "\\d{3}\\w{10}\\d{2}"), // Macedonia
-    MR(23, "\\d{5}\\d{5}\\d{11}\\d{2}"), // Mauritania
-    MT(27, "[A-Z]{4}\\d{5}\\w{18}"), // Malta
-    MU(26, "[A-Z]{4}\\d{2}\\d{2}\\d{12}\\d{3}\\w{3}"), // Mauritius
-    NL(14, "[A-Z]{4}\\d{10}"), // Netherlands 
-    NO(11, "\\d{4}\\d{6}\\d"), // Norway
-    PK(20, "[A-Z]{4}\\w{16}"), // Pakistan
-    PL(24, "\\d{8}\\d{16}"), // Poland
-    PS(25, "[A-Z]{4}\\w{21}"), // Palestine
-    PT(21, "\\d{4}\\d{4}\\d{11}\\d{2}"), // Portugal
-    QA(25, "[A-Z]{4}\\w{21}"), // Qatar
-    RO(20, "[A-Z]{4}\\w{16}"), // Romania
-    RS(18, "\\d{3}\\d{13}\\d{2}"), // Serbia
-    SA(20, "\\d{2}\\w{18}"), // Saudi Arabia
-    SC(27, "[A-Z]{4}\\d{2}\\d{2}\\d{16}[A-Z]{3}"), // Seychelles
-    SE(20, "\\d{3}\\d{16}\\d"), // Sweden
-    SI(15, "\\d{5}\\d{8}\\d{2}"), // Slovenia
-    SK(20, "\\d{4}\\d{6}\\d{10}"), // Slovakia
-    SM(23, "[A-Z]\\d{5}\\d{5}\\w{12}"), // San Marino
-    ST(21, "\\d{4}\\d{4}\\d{11}\\d{2}"), // Sao Tome and Principe
-    SV(24, "[A-Z]{4}\\d{20}"), // El Salvador
-    TL(19, "\\d{3}\\d{14}\\d{2}"), // East Timor
-    TN(20, "\\d{2}\\d{3}\\d{13}\\d{2}"), // Tunisia
-    TR(22, "\\d{5}\\d\\w{16}"), // Turkey
-    UA(25, "\\d{6}\\w{19}"), // Ukraine
-    VG(20, "[A-Z]{4}\\d{16}"), // Virgin Islands
-    XK(16, "\\d{4}\\d{10}\\d{2}"); // Kosovo
+	/** Andorra */
+    AD(20, "\\d{4}", "\\d{4}", "\\w{12}"),
     
+    /** United Arab Emirates */
+    AE(19, "\\d{3}", "\\d{16}"),
+    
+    /** Albania */
+    AL(24, "\\d{8}", "\\w{16}"), // the spec is unclear about the bank and branch IDs - states 8 as total 
+    // length, but the spec only notes 7 digits total for these two (Bank: 1-3, Branch: 4-7). We assume the last digit
+    // is some sort of check digit, but it's not properly specified - so until that's clear, we can't support separate
+    // IDs
+    
+    /** Austria */
+    AT(16, "\\d{5}", "\\d{11}"),
+    
+    /** Azerbaijan */
+    AZ(24, "[A-Z]{4}", "\\w{20}"),
+    
+    /** Bosnia and Herzegovina */
+    BA(16, "\\d{3}", "\\d{3}", "\\d{8}\\d{2}"),
+    
+    /** Belgium */
+    BE(12, "\\d{3}", "\\d{7}\\d{2}"),
+    
+    /** Bulgaria */
+    BG(18, "[A-Z]{4}", "\\d{4}", "\\d{2}\\w{8}"),
+    
+    /** Bahrain */
+    BH(18, "[A-Z]{4}", "\\w{14}"),
+    
+    /** Brazil */
+    BR(25, "\\d{8}", "\\d{5}", "\\d{10}[A-Z]\\w"),
+    
+    /** Belarus */
+    BY(24, "\\w{4}", "\\d{4}\\w{16}"),
+    
+    /** Switzerland */
+    CH(17, "\\d{5}", "\\w{12}"),
+    
+    /** Costa Rica */
+    CR(18, "\\d{4}", "\\d{14}"),
+    
+    /** Cyprus */
+    CY(24, "\\d{3}", "\\d{5}", "\\w{16}"),
+    
+    /** Czech Republic */
+    CZ(20, "\\d{4}", "\\d{6}\\d{10}"),
+    
+    /** Germany */
+    DE(18, "\\d{8}", "\\d{10}"),
+    
+    /** Denmark */
+    DK(14, "\\d{4}", "\\d{9}\\d{1}"),
+    
+    /** Dominican Republic */
+    DO(24, "\\w{4}", "\\d{20}"),
+    
+    /** Estonia */
+    EE(16, "\\d{2}", "\\d{2}\\d{11}\\d"),
+    
+    /** Spain */
+    ES(20, "\\d{4}", "\\d{4}", "\\d\\d\\d{10}"),
+    
+    /** Finland */
+    FI(14, "\\d{3}", "\\d{11}"),
+    
+    /** Faroe Islands */
+    FO(14, "\\d{4}", "\\d{9}\\d"),
+    
+    /** France */
+    FR(23, "\\d{5}", "\\d{5}\\w{11}\\d{2}"),
+    
+    /** United Kingdom */
+    GB(18, "[A-Z]{4}", "\\d{6}", "\\d{8}"),
+    
+    /** Georgia */
+    GE(18, "[A-Z]{2}", "\\d{16}"),
+    
+    /** Gibraltar */
+    GI(19, "[A-Z]{4}", "\\w{15}"),
+    
+    /** Greenland */
+    GL(14, "\\d{4}", "\\d{9}\\d"),
+    
+    /** Greece */
+    GR(23, "\\d{3}", "\\d{4}", "\\w{16}"),
+    
+    /** Guatemala */
+    GT(24, "\\w{4}", "\\w{20}"),
+    
+    /** Croatia */
+    HR(17, "\\d{7}", "\\d{10}"),
+    
+    /** Hungary */
+    HU(24, "\\d{3}", "\\d{4}", "\\d\\d{15}\\d"),
+    
+    /** Ireland */
+    IE(18, "[A-Z]{4}", "\\d{6}", "\\d{8}"),
+    
+    /** Israel */
+    IL(19, "\\d{3}", "\\d{3}", "\\d{13}"),
+    
+    /** Iraq */
+    IQ(19, "[A-Z]{4}", "\\d{3}", "\\d{12}"),
+    
+    /** Iceland */
+    IS(22, "\\d{2}", "\\d{2}", "\\d{2}\\d{6}\\d{10}"),
+    
+    /** Italy */
+    IT(23, "[A-Z]", "\\d{5}", "\\d{5}", "\\w{12}"),
+    
+    /** Jordan */
+    JO(26, "[A-Z]{4}", "\\d{4}\\w{18}"),
+    
+    /** Kuwait */
+    KW(26, "[A-Z]{4}", "\\w{22}"),
+    
+    /** Kazakhstan */
+    KZ(16, "\\d{3}", "\\w{13}"),
+    
+    /** Lebanon */
+    LB(24, "\\d{4}", "\\w{20}"),
+    
+    /** Saint Lucia */
+    LC(28, "[A-Z]{4}", "\\w{24}"),
+    
+    /** Liechtenstein */
+    LI(17, "\\d{5}", "\\w{12}"),
+    
+    /** Lithuania */
+    LT(16, "\\d{5}", "\\d{11}"),
+    
+    /** Luxembourg */
+    LU(16, "\\d{3}", "\\w{13}"),
+    
+    /** Latvia */
+    LV(17, "[A-Z]{4}", "\\w{13}"),
+
+    /** Monaco */
+    MC(23, "\\d{5}", "\\d{5}", "\\w{11}\\d{2}"),
+
+    /** Moldova */
+    MD(20, "\\w{2}", "\\w{18}"),
+
+    /** Montenegro */
+    ME(18, "\\d{3}", "\\d{13}\\d{2}"),
+    
+    /** Macedonia */
+    MK(15, "\\d{3}", "\\w{10}\\d{2}"),
+    
+    /** Mauritania */
+    MR(23, "\\d{5}", "\\d{5}", "\\d{11}\\d{2}"),
+    
+    /** Malta */
+    MT(27, "[A-Z]{4}", "\\d{5}", "\\w{18}"),
+    
+    /** Mauritius */
+    MU(26, "[A-Z]{4}\\d{2}", "\\d{2}", "\\d{12}\\d{3}\\w{3}"),
+    
+    /** The Netherlands */
+    NL(14, "[A-Z]{4}", "\\d{10}"),
+    
+    /** Norway */
+    NO(11, "\\d{4}", "\\d{6}\\d"),
+    
+    /** Pakistan */
+    PK(20, "[A-Z]{4}", "\\w{16}"),
+    
+    /** Poland */
+    PL(24, "\\d{8}", "\\d{16}"),
+    
+    /** Palestine */
+    PS(25, "[A-Z]{4}", "\\w{21}"),
+
+    /** Portugal */
+    PT(21, "\\d{4}", "\\d{4}\\d{11}\\d{2}"),
+    
+    /** Qatar */
+    QA(25, "[A-Z]{4}", "\\w{21}"),
+
+    /** Romania */
+    RO(20, "[A-Z]{4}", "\\w{16}"),
+
+    /** Serbia */
+    RS(18, "\\d{3}", "\\d{13}\\d{2}"),
+
+    /** Saudi Arabia */
+    SA(20, "\\d{2}", "\\w{18}"),
+
+    /** Seychelles */
+    SC(27, "[A-Z]{4}\\d{2}", "\\d{2}", "\\d{16}[A-Z]{3}"),
+
+    /** Sweden */
+    SE(20, "\\d{3}", "\\d{16}\\d"),
+    
+    /** Slovenia */
+    SI(15, "\\d{5}", "\\d{8}\\d{2}"),
+    
+    /** Slovakia */
+    SK(20, "\\d{4}", "\\d{6}\\d{10}"),
+
+    /** San Marino */
+    SM(23, "[A-Z]", "\\d{5}", "\\d{5}", "\\w{12}"),
+
+    /** Sao Tome and Principe */
+    ST(21, "\\d{4}", "\\d{4}", "\\d{11}\\d{2}"),
+
+    /** El Salvador */
+    SV(24, "[A-Z]{4}", "\\d{20}"),
+
+    /** East Timor */
+    TL(19, "\\d{3}", "\\d{14}\\d{2}"),
+
+    /** Tunisia */
+    TN(20, "\\d{2}", "\\d{3}", "\\d{13}\\d{2}"),
+    
+    /** Turkey */
+    TR(22, "\\d{5}", "\\d\\w{16}"),
+
+    /** Ukraine */
+    UA(25, "\\d{6}", "\\w{19}"),
+    
+    /** Virgin Islands */
+    VG(20, "[A-Z]{4}", "\\d{16}"),
+    
+    /** Kosovo */
+    XK(16, "\\d{2}", "\\d{2}", "\\d{10}\\d{2}");
+    
+	/** Common IBAN check digits pattern. */
+	private static final String CHECK_DIGITS_PATTERN = "\\d{2}";
+	
 	/** Country-specific BBAN length. */
     private int bbanLength;
 
     /** Regular Expression of the BBAN pattern for this country code. */
     private String bbanPattern;
+
+    /** Regular Expression of the IBAN pattern for this country code. */
+    private String ibanPattern;
+    
+    /** Optional BBAN prefix pattern (e.g. checksum in Italy). */
+    private String bbanPrefixPattern;
+    
+    /** Bank identifier pattern. */
+    private String bankIdentifierPattern;
+    
+    /** Optional branch identifier pattern. */
+    private String branchIdentifierPattern;
+    
+    /** Account number pattern for this country code. */
+    private String accountNumberPattern;
     
     /**
-     * @param length country-specific BBAN length
-     * @param bbanPattern regular expression describing this country code's BBAN format
-     */
-    private CountryCode(int bbanLength, String bbanPattern) {
-        this.bbanLength = bbanLength;
-        this.bbanPattern = bbanPattern;
-    }
+	 * @param bbanLength
+	 * @param bankIdentifierPattern
+	 * @param accountNumberPattern
+	 */
+	private CountryCode(int bbanLength, String bankIdentifierPattern, String accountNumberPattern) {
+		this(bbanLength, bankIdentifierPattern, null, accountNumberPattern);
+	}
+	
+	/**
+	 * @param bbanLength
+	 * @param bankIdentifierPattern
+	 * @param branchIdentifierPattern
+	 * @param accountNumberPattern
+	 */
+	private CountryCode(int bbanLength, String bankIdentifierPattern, String branchIdentifierPattern,
+			String accountNumberPattern) {
+		this(bbanLength, null, bankIdentifierPattern, branchIdentifierPattern, accountNumberPattern);
+	}
 
-    /**
+	/**
+	 * @param bbanLength
+	 * @param bbanPrefixPattern
+	 * @param bankIdentifierPattern
+	 * @param branchIdentifierPattern
+	 * @param accountNumberPattern
+	 */
+	private CountryCode(int bbanLength, String bbanPrefixPattern, String bankIdentifierPattern, String branchIdentifierPattern,
+			String accountNumberPattern) {
+		this.bbanLength = bbanLength;
+		this.bbanPrefixPattern = bbanPrefixPattern;
+		this.bankIdentifierPattern = bankIdentifierPattern;
+		this.branchIdentifierPattern = branchIdentifierPattern;
+		this.accountNumberPattern = accountNumberPattern;
+		
+		final StringBuilder sb = new StringBuilder();
+		if (bbanPrefixPattern != null) {
+			sb.append(bbanPrefixPattern);
+		}
+		
+		sb.append(bankIdentifierPattern);
+		
+		if (branchIdentifierPattern != null) {
+			sb.append(branchIdentifierPattern);
+		}
+		
+		sb.append(accountNumberPattern);
+		
+		this.bbanPattern = sb.toString();
+		this.ibanPattern = name() + CHECK_DIGITS_PATTERN + bbanPattern;
+	}
+	
+	/**
      * 
      * @return the length of a BBAN of this country
      */
@@ -138,9 +351,55 @@ public enum CountryCode {
      * @return a regular expression describing this country code's IBAN format
      */
     public String getIbanPattern() {
-    	return this.name()+"\\d{2}"+bbanPattern;
+    	return ibanPattern;
     }
     
+    /**
+	 * @return the bbanPrefixPattern
+	 */
+	public String getBbanPrefixPattern() {
+		return bbanPrefixPattern;
+	}
+
+	/**
+	 * 
+	 */
+	public boolean hasBbanPrefix() {
+		return bbanPrefixPattern != null;
+	}
+	
+	/**
+     * 
+     * @return
+     */
+	public String getBankIdentifierPattern() {
+		return bankIdentifierPattern;
+	}
+
+	/**
+	 * 
+	 * @return may return <code>null</code>
+	 */
+	public String getBranchIdentifierPattern() {
+		return branchIdentifierPattern;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean hasBranchIdentifier() {
+		return branchIdentifierPattern != null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getAccountNumberPattern() {
+		return accountNumberPattern;
+	}
+
 	/**
      * This method simply calls {@link #valueOf(String)} but wraps any {@link IllegalArgumentException} that may be
      * thrown there.
--- src/test/java/de/tfsw/ibanlib/CountryCodeTest.java
+++ src/test/java/de/tfsw/ibanlib/CountryCodeTest.java
@@ -20,42 +20,42 @@ public class CountryCodeTest {
 
 	@Test
 	public void test() {
-		doTestCountryCode(CountryCode.AD, 20, "00012030200359100100", "AD1200012030200359100100");
+		doTestCountryCode(CountryCode.AD, 20, "00012030200359100100", "AD1200012030200359100100", true);
 		doTestCountryCode(CountryCode.AE, 19, "0331234567890123456", "AE070331234567890123456");
-		doTestCountryCode(CountryCode.AL, 24, "212110090000000235698741", "AL47212110090000000235698741");
+		doTestCountryCode(CountryCode.AL, 24, "212110090000000235698741", "AL47212110090000000235698741", false);
 		doTestCountryCode(CountryCode.AT, 16, "1904300234573201", "AT611904300234573201");
 		doTestCountryCode(CountryCode.AZ, 24, "NABZ00000000137010001944", "AZ21NABZ00000000137010001944");
-		doTestCountryCode(CountryCode.BA, 16, "1990440001200279", "BA391290079401028494");
+		doTestCountryCode(CountryCode.BA, 16, "1990440001200279", "BA391290079401028494", true);
 		doTestCountryCode(CountryCode.BE, 12, "539007547034", "BE68539007547034");
-		doTestCountryCode(CountryCode.BG, 18, "BNBG96611020345678", "BG80BNBG96611020345678");
+		doTestCountryCode(CountryCode.BG, 18, "BNBG96611020345678", "BG80BNBG96611020345678", true);
 		doTestCountryCode(CountryCode.BH, 18, "BMAG00001299123456", "BH67BMAG00001299123456");
-		doTestCountryCode(CountryCode.BR, 25, "00360305000010009795493P1", "BR1800360305000010009795493C1");
+		doTestCountryCode(CountryCode.BR, 25, "00360305000010009795493P1", "BR1800360305000010009795493C1", true);
 		doTestCountryCode(CountryCode.BY, 24, "NBRB3600000000000Z00AB00", "BY13NBRB3600900000002Z00AB00");
 		doTestCountryCode(CountryCode.CH, 17, "00762011623852957", "CH9300762011623852957");
 		doTestCountryCode(CountryCode.CR, 18, "015202001026284066", "CR05015202001026284066");
-		doTestCountryCode(CountryCode.CY, 24, "002001280000001200527600", "CY17002001280000001200527600");
+		doTestCountryCode(CountryCode.CY, 24, "002001280000001200527600", "CY17002001280000001200527600", true);
 		doTestCountryCode(CountryCode.CZ, 20, "08000000192000145399", "CZ6508000000192000145399");
 		doTestCountryCode(CountryCode.DE, 18, "370400440532013000", "DE89370400440532013000");
 		doTestCountryCode(CountryCode.DK, 14, "00400440116243", "DK5000400440116243");
 		doTestCountryCode(CountryCode.DO, 24, "BAGR00000001212453611324", "DO28BAGR00000001212453611324");
 		doTestCountryCode(CountryCode.EE, 16, "2200221020145685", "EE382200221020145685");
-		doTestCountryCode(CountryCode.ES, 20, "21000418450200051332", "ES9121000418450200051332");
+		doTestCountryCode(CountryCode.ES, 20, "21000418450200051332", "ES9121000418450200051332", true);
 		doTestCountryCode(CountryCode.FI, 14, "12345600000785", "FI2112345600000785");
 		doTestCountryCode(CountryCode.FO, 14, "64600001631634", "FO6264600001631634");
 		doTestCountryCode(CountryCode.FR, 23, "20041010050500013M02606", "FR1420041010050500013M02606");
-		doTestCountryCode(CountryCode.GB, 18, "NWBK60161331926819", "GB29NWBK60161331926819");
+		doTestCountryCode(CountryCode.GB, 18, "NWBK60161331926819", "GB29NWBK60161331926819", true);
 		doTestCountryCode(CountryCode.GE, 18, "NB0000000101904917", "GE29NB0000000101904917");
 		doTestCountryCode(CountryCode.GI, 19, "NWBK000000007099453", "GI75NWBK000000007099453");
 		doTestCountryCode(CountryCode.GL, 14, "64710001000206", "GL8964710001000206");
-		doTestCountryCode(CountryCode.GR, 23, "01101250000000012300695", "GR1601101250000000012300695");
+		doTestCountryCode(CountryCode.GR, 23, "01101250000000012300695", "GR1601101250000000012300695", true);
 		doTestCountryCode(CountryCode.GT, 24, "TRAJ01020000001210029690", "GT82TRAJ01020000001210029690");
 		doTestCountryCode(CountryCode.HR, 17, "10010051863000160", "HR1210010051863000160");
-		doTestCountryCode(CountryCode.HU, 24, "117730161111101800000000", "HU42117730161111101800000000");
-		doTestCountryCode(CountryCode.IE, 18, "AIBK93115212345678", "IE29AIBK93115212345678");
-		doTestCountryCode(CountryCode.IL, 19, "0108000000099999999", "IL620108000000099999999");
-		doTestCountryCode(CountryCode.IQ, 19, "NBIQ850123456789012", "IQ98NBIQ850123456789012");
-		doTestCountryCode(CountryCode.IS, 22, "0159260076545510730339", "IS140159260076545510730339");
-		doTestCountryCode(CountryCode.IT, 23, "X0542811101000000123456", "IT60X0542811101000000123456");
+		doTestCountryCode(CountryCode.HU, 24, "117730161111101800000000", "HU42117730161111101800000000",true);
+		doTestCountryCode(CountryCode.IE, 18, "AIBK93115212345678", "IE29AIBK93115212345678", true);
+		doTestCountryCode(CountryCode.IL, 19, "0108000000099999999", "IL620108000000099999999", true);
+		doTestCountryCode(CountryCode.IQ, 19, "NBIQ850123456789012", "IQ98NBIQ850123456789012", true);
+		doTestCountryCode(CountryCode.IS, 22, "0159260076545510730339", "IS140159260076545510730339", true);
+		doTestCountryCode(CountryCode.IT, 23, "X0542811101000000123456", "IT60X0542811101000000123456", true);
 		doTestCountryCode(CountryCode.JO, 26, "CBJO0010000000000131000302", "JO94CBJO0010000000000131000302");
 		doTestCountryCode(CountryCode.KW, 26, "CBKU0000000000001234560101", "KW81CBKU0000000000001234560101");
 		doTestCountryCode(CountryCode.KZ, 16, "125KZT5004100100", "KZ86125KZT5004100100");
@@ -65,13 +65,13 @@ public class CountryCodeTest {
 		doTestCountryCode(CountryCode.LT, 16, "1000011101001000", "LT121000011101001000");
 		doTestCountryCode(CountryCode.LU, 16, "0019400644750000", "LU280019400644750000");
 		doTestCountryCode(CountryCode.LV, 17, "BANK0000435195001", "LV80BANK0000435195001");
-		doTestCountryCode(CountryCode.MC, 23, "11222000010123456789030", "MC5811222000010123456789030");
+		doTestCountryCode(CountryCode.MC, 23, "11222000010123456789030", "MC5811222000010123456789030", true);
 		doTestCountryCode(CountryCode.MD, 20, "AG000225100013104168", "MD24AG000225100013104168");
 		doTestCountryCode(CountryCode.ME, 18, "505000012345678951", "ME25505000012345678951");
 		doTestCountryCode(CountryCode.MK, 15, "250120000058984", "MK07250120000058984");
-		doTestCountryCode(CountryCode.MR, 23, "00020001010000123456753", "MR1300020001010000123456753");
-		doTestCountryCode(CountryCode.MT, 27, "MALT011000012345MTLCAST001S", "MT84MALT011000012345MTLCAST001S");
-		doTestCountryCode(CountryCode.MU, 26, "BOMM0101101030300200000MUR", "MU17BOMM0101101030300200000MUR");
+		doTestCountryCode(CountryCode.MR, 23, "00020001010000123456753", "MR1300020001010000123456753", true);
+		doTestCountryCode(CountryCode.MT, 27, "MALT011000012345MTLCAST001S", "MT84MALT011000012345MTLCAST001S", true);
+		doTestCountryCode(CountryCode.MU, 26, "BOMM0101101030300200000MUR", "MU17BOMM0101101030300200000MUR", true);
 		doTestCountryCode(CountryCode.NL, 14, "ABNA0417164300", "NL91ABNA0417164300");
 		doTestCountryCode(CountryCode.NO, 11, "86011117947", "NO9386011117947");
 		doTestCountryCode(CountryCode.PK, 20, "SCBL0000001123456702", "PK36SCBL0000001123456702");
@@ -82,27 +82,32 @@ public class CountryCodeTest {
 		doTestCountryCode(CountryCode.RO, 20, "AAAA1B31007593840000", "RO49AAAA1B31007593840000");
 		doTestCountryCode(CountryCode.RS, 18, "260005601001611379", "RS35260005601001611379");
 		doTestCountryCode(CountryCode.SA, 20, "80000000608010167519", "SA0380000000608010167519");
-		doTestCountryCode(CountryCode.SC, 27, "SSCB11010000000000001497USD", "SC18SSCB11010000000000001497USD");
+		doTestCountryCode(CountryCode.SC, 27, "SSCB11010000000000001497USD", "SC18SSCB11010000000000001497USD", true);
 		doTestCountryCode(CountryCode.SE, 20, "50000000058398257466", "SE4550000000058398257466");
 		doTestCountryCode(CountryCode.SI, 15, "263300012039086", "SI56263300012039086");
 		doTestCountryCode(CountryCode.SK, 20, "12000000198742637541", "SK3112000000198742637541");
-		doTestCountryCode(CountryCode.SM, 23, "U0322509800000000270100", "SM86U0322509800000000270100");
-		doTestCountryCode(CountryCode.ST, 21, "000200010192194210112", "ST68000200010192194210112");
+		doTestCountryCode(CountryCode.SM, 23, "U0322509800000000270100", "SM86U0322509800000000270100", true);
+		doTestCountryCode(CountryCode.ST, 21, "000200010192194210112", "ST68000200010192194210112", true);
 		doTestCountryCode(CountryCode.SV, 24, "CENR00000000000000700025", "SV62CENR00000000000000700025");
 		doTestCountryCode(CountryCode.TL, 19, "0080012345678910157", "TL380080012345678910157");
-		doTestCountryCode(CountryCode.TN, 20, "10006035183598478831", "TN5910006035183598478831");
+		doTestCountryCode(CountryCode.TN, 20, "10006035183598478831", "TN5910006035183598478831", true);
 		doTestCountryCode(CountryCode.TR, 22, "0006100519786457841326", "TR330006100519786457841326");
 		doTestCountryCode(CountryCode.UA, 25, "3223130000026007233566001", "UA213223130000026007233566001");
 		doTestCountryCode(CountryCode.VG, 20, "VPVG0000012345678901", "VG96VPVG0000012345678901");
-		doTestCountryCode(CountryCode.XK, 16, "1212012345678906", "XK051212012345678906");
+		doTestCountryCode(CountryCode.XK, 16, "1212012345678906", "XK051212012345678906", true);
+	}
+
+	private void doTestCountryCode(CountryCode code, int expectedBbanLength, String bbanToTest, String ibanToTest) {
+		doTestCountryCode(code, expectedBbanLength, bbanToTest, ibanToTest, false);
 	}
 	
-	private void doTestCountryCode(CountryCode code, int expectedBbanLength, String bbanToTest, String ibanToTest) {
+	private void doTestCountryCode(CountryCode code, int expectedBbanLength, String bbanToTest, String ibanToTest, boolean hasBranchCode) {
 		assertEquals(
 				String.format("Country Code %s does not have expected BBAN length: %d, instead: %d", code.toString(), expectedBbanLength, code.getBbanLength()), 
 				expectedBbanLength, code.getBbanLength());
-		assertTrue("Country Code BBAN pattern error: " + code.toString(), bbanToTest.matches(code.getBbanPattern()));
+		assertTrue("Country Code BBAN pattern error: " + code.toString() + " : " + code.getBbanPattern(), bbanToTest.matches(code.getBbanPattern()));
 		assertTrue("Country Code IBAN pattern error: " + code.toString(), ibanToTest.matches(code.getIbanPattern()));
+		assertEquals("Country Code Branch Code mismatch: " + code.toString(), hasBranchCode, code.hasBranchIdentifier());
 	}
 }
 
